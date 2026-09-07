aa2995c0a577395a9d71943649daa09a0bec158d
Zero <zerob4e@gmail.com>
2026-09-06 22:32:52 -0300

    Delete ABRIR MIRROR.bat

--- ABRIR MIRROR.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-start "MIRROR-07 LOCAL RELAY" /min powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0server.ps1" -Port 8765
-ping 127.0.0.1 -n 2 >nul
-start "" "http://localhost:8765/"
-exit /b 0
